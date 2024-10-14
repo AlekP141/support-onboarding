bcad294ae710cf1e722eca3b554fbe82da630274
Alek Pialucha <pialuchaa@btinternet.com>
2024-10-14 20:41:24 +0100

    applications list

--- src/components/AppInfoBar/AppInfoBar.jsx
+++ src/components/AppInfoBar/AppInfoBar.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { QuestionContext } from "../../App";
+import { APPLICATION_LIST } from "../consts";
 
 export const AppInfoBar = () => {
   const question = useContext(QuestionContext);
+
   return (
     <div className="app-info-bar">
       <h2>Apps</h2>
@@ -42,12 +44,31 @@ export const AppInfoBar = () => {
               <a href="">Admin link</a>
             </div>
           </div>
+          <div className="algolia-perso-indicator">
+            <img src="https://819835.apps.zdusercontent.com/819835/assets/1707464129-4e56f77cdbf4d416015fc01216d6b0e0/icons/check-circle-solid.svg" alt="" />
+            <p>User can be personified.</p>
+          </div>
+          <ul className="algolia-user-info-list">
+            <li>
+              <strong>Created at:</strong> Oct 14, 2024
+            </li>
+          </ul>
         </div>
         <div className="algolia-applications-container">
           <div className="algolia-app-subtitle">
             <img src="https://819835.apps.zdusercontent.com/819835/assets/1707464129-4e56f77cdbf4d416015fc01216d6b0e0/icons/folder-open-solid.svg" alt="" />
             <h3>Applications</h3>
           </div>
+          <ul className="algolia-applications-list">
+            {APPLICATION_LIST.map((app) => {
+              return (
+                <a key={app.appID} href={`https://admin.algolia.com/admin/users/810007/applications/${app.appID}`}>
+                  <li>{app.appID}</li>
+                  {app.type === "shopify" ? <div className="shopify" /> : null}
+                </a>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </div>
