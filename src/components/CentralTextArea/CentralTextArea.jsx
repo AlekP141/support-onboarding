ff3cee2bdb39dabb73a3ab51ea6be94f81985c2d
Alek Pialucha <pialuchaa@btinternet.com>
2024-10-14 22:59:42 +0100

    initial CentralTextArea structure

--- src/components/CentralTextArea/CentralTextArea.jsx
+++ src/components/CentralTextArea/CentralTextArea.jsx
@@ -3,12 +3,15 @@ import { QuestionContext } from "../../App"
 
 export const CentralTextArea = () => {
   const [question, setQuestion] = useContext(QuestionContext)
-  console.log(question)
+
   return (
     <div className="text-area-structure">
       <div className="text-area-header">{question.title}</div>
       <div className="text-area-container"></div>
-      <div className="text-area-reply"></div>
+      <div className="text-area-reply">
+        <div className="text-area-reply-header"></div>
+        <textarea name="replyArea" id=""></textarea>
+      </div>
     </div>
   )
 }
