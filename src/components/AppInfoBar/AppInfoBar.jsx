7462e143533e873ef2431c94d6f5479d2cbf13cb
Alek Pialucha <pialuchaa@btinternet.com>
2024-10-14 15:27:33 +0100

    AppInfoBar structure

---
+++ src/components/AppInfoBar/AppInfoBar.jsx
@@ -0,0 +1,16 @@
+export const AppInfoBar = () => {
+
+  return (
+    <div className="app-info-bar">
+      <div className="app-info-container">
+        <div className="algolia-account-overview">
+          <img src="" alt="" />
+          <p>Algolia Account Overview</p>
+        </div>
+        <div className="algolia-org"></div>
+        <div className="algolia-user-info"></div>
+        <div className="algolia-applications-container"></div>
+      </div>
+    </div>
+  )
+}
