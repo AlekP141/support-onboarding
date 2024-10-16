cf5b70f9140234f8e4f1c42ed3913b8789ac4cb0
Alek Pialucha <pialuchaa@btinternet.com>
2024-10-16 19:00:52 +0100

    ticket conversation template functional

--- src/components/CentralTextArea/RenderMessage/RenderMessage.jsx
+++ src/components/CentralTextArea/RenderMessage/RenderMessage.jsx
@@ -1,19 +1,21 @@
-const RenderMessage = ({message}) => {
-
-  const {sender, text} = message
+const RenderMessage = ({ message }) => {
+  const { sender, text } = message;
 
   return (
-    <div className="message-layout-grid">
-      <div className={`message-avatar ${sender === "customer" ? "customer-avatar" : "support-avatar"}`}>
-
-      </div>
+    <div className="message-layout">
       <div className="message-header">
-        <div className="message-sender-info"></div>
-        <div className="message-recipient"></div>
+        <div className={`message-avatar ${sender === "customer" ? "customer-avatar" : "support-avatar"}`}></div>
+        <div className="message-sender-info">
+          <div className="message-sender-name">{sender === "customer" ? "Temp Cust Name" : "Algolia Support"}</div>
+          <div className="message-recipient">
+            <strong>To:</strong>
+            <span className="message-recipient-name">{sender === "customer" ? "Algolia Support" : "Temp Cust Name"}</span>
+          </div>
+        </div>
       </div>
-      <div className={`message-body ${sender === "customer" ? "customer-message" : "support-message"}`}>{text}</div>
+      <p className={`message-body ${sender === "customer" ? "customer-message" : "support-message"}`}>{text}</p>
     </div>
-  )
-}
+  );
+};
 
-export default RenderMessage
+export default RenderMessage;
