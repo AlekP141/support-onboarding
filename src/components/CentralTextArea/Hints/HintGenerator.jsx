51a3806a6b36055ff3b29a4356b7aa102d95298f
Alek Pialucha <pialuchaa@btinternet.com>
2024-10-23 11:53:20 +0100

    hint tracker in url

--- src/components/CentralTextArea/Hints/HintGenerator.jsx
+++ src/components/CentralTextArea/Hints/HintGenerator.jsx
@@ -1,5 +1,5 @@
 import { QuestionContext } from "../../../App";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 const HintGenerator = ({ setCurrentHints, currentHints }) => {
   const [question] = useContext(QuestionContext);
@@ -9,6 +9,19 @@ const HintGenerator = ({ setCurrentHints, currentHints }) => {
   const generateHint = () => {
     setCurrentHints((prev) => [...prev, availableHints[prev.length]]);
   };
+
+  useEffect(() => {
+    let url = window.location.href;
+    const regex = /\/h\d+\//;
+
+    if (regex.test(url)) {
+      url = url.replace(regex, `/h${currentHints.length}/`);
+    } else {
+      url = `${url}h1/`;
+    }
+
+    window.history.pushState({}, "", url);
+  }, [currentHints]);
 
   if (currentHints.length < availableHints.length) {
     return (
@@ -22,3 +35,9 @@ const HintGenerator = ({ setCurrentHints, currentHints }) => {
 };
 
 export default HintGenerator;
+
+// TEST TO SEE HOW TO APPEND INFO TO URL ON SAME PAGE - WORKING
+// const url = window.location.href;
+// const newUrl = url.includes('?a') ? `${url}?a` : `${url}`;
+// window.history.pushState({}, "", newUrl);
+// console.log(newUrl);
